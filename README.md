f1a53706333ee7ed9fb7faf80bdb7f36a8dabed9
riggy420 <riggyatwork@gmail.com>
2026-06-27 11:31:37 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ITSO 
+
+## How to start the server
+
+
+## Architure
+
+Backend (Gin), Database (influxDB)
+
+Frontend(React)
+
---
+++ cmd/lorawan/start.go
@@ -0,0 +1,32 @@
+package lorwan
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"context"
+)
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start the LoRaWAN server",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Starting LoRaWAN server...")
+		// Add your server start logic here
+		startServer()
+	},
+}
+
+func startServer() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel() 
+
+	// waitgroup to track goroutines
+	var wg sync.WaitGroup
+
+	//status channel to receive status updates from goroutines
+	statusChan := make(chan string)
+
+	// print Something
+	fmt.Println("Starting LoRaWAN server...")
+
+}
